2237c48ca65c57113a7b2faa74eabd24027fdfc1
Patrick Walsh <34382502+PatrickWalsh6079@users.noreply.github.com>
2020-03-04 16:34:42 -0500

    Add files via upload

---
+++ JS_basics/Data Types.js
@@ -0,0 +1,9 @@
+// DATA TYPES
+
+
+var length = 16; // Number
+var lastName = "Johnson"; // String
+var x = {
+    firstName: "Joe",
+    lastName: "Schmoe"
+}; // Object
---
+++ JS_basics/Strings.js
@@ -0,0 +1,16 @@
+// STRINGS
+
+
+// find length of String with length property
+var txt = "Hello World!";
+var x = txt.length;
+console.log(x);
+
+// use escape characters for quotes or other special characters inside String
+var txt = "We are the \"Vikings\".";
+console.log(txt);
+
+// concatenate two Strings
+var str1 = "Hello ";
+var str2 = "World!";
+console.log(str1 + str2);
